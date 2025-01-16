e23bace544732e600cc89345116713a3dd068ebb
Felipe Morais <91854957+FelipeTr00@users.noreply.github.com>
2025-01-16 16:26:11 -0300

    Update webDriver

--- webDriverConf.py
+++ webDriverConf.py
@@ -1,16 +1,32 @@
 from selenium import webdriver
 
-def configure_driver():
+# Configuração para Microsoft Edge
     options = webdriver.EdgeOptions()
-    # options = webdriver.ChromeOptions()
-    # options = webdriver.FirefoxOptions()
-
-    # Configurações
+    
+    # Configurações gerais
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-software-rasterizer")
     options.add_argument("--start-maximized")
-    # options.add_argument("--window-size=960,1080")
-    # options.add_argument("--window-position=0,0")
     options.add_argument("--disable-cache")
     options.add_argument("--disable-application-cache")
     return webdriver.Edge(options=options)
+
+# Configuração para Google Chrome
+#     options = webdriver.ChromeOptions()
+    
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--disable-software-rasterizer")
+#     options.add_argument("--start-maximized")
+#     options.add_argument("--disable-cache")
+#     options.add_argument("--disable-application-cache")
+#     return webdriver.Chrome(options=options)
+
+# Configuração para Mozilla Firefox
+#     options = webdriver.FirefoxOptions()
+    
+#     options.add_argument("--disable-gpu")
+#     options.set_preference("browser.cache.disk.enable", False)
+#     options.set_preference("browser.cache.memory.enable", False)
+#     options.set_preference("browser.cache.offline.enable", False)
+#     options.set_preference("network.http.use-cache", False)
+#     return webdriver.Firefox(options=options)
